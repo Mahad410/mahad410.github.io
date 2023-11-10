149e8e7fb5586501f911ebdcdb0b78900416af06
Mahad <mahadqureshi410@gmail.com>
2023-11-10 09:44:15 +0500

    Update data_projects.js

--- data_projects.js
+++ data_projects.js
@@ -1,138 +1,138 @@
 const data_projects = [{
   name: "Snap landing page",
-  url: "https://mahidqureshi.me/snap",
+  url: "https://mahad410.github.io/snap",
   description: "Snap landing page with drop-down menu",
   img: "/pics/projects/snap.webp"
 },
   {
   name: "Modern News concept page",
-  url: "https://mahidqureshi.me/news",
+  url: "https://mahad410.github.io/news",
   description: "Modern news site concept",
   img: "/pics/projects/news.webp"
 },
   {
     name: "Credit Card Checkout",
-    url: "https://mahidqureshi.me/credit_card",
+    url: "https://mahad410.github.io/credit_card",
     description: "Interactive credit card form with validation",
     img: "/pics/projects/credit_card.webp"
   },
   {
     name: "Login page concept",
-    url: "https://mahidqureshi.me/login",
+    url: "https://mahad410.github.io/login",
     description: "Modern login page concept",
     img: "/pics/projects/login.webp"
   },
   {
   name: "Coming soon page",
-  url: "https://mahidqureshi.me/comingsoon",
+  url: "https://mahad410.github.io/comingsoon",
   description: "Coming soon page with newsletter",
   img: "/pics/projects/comingsoon.webp"
 },
   {
     name: "FAQ Component",
-    url: "https://mahidqureshi.me/faq",
+    url: "https://mahad410.github.io/faq",
     description: "FAQ component with toggle without JavaScript",
     img: "/pics/projects/faq.webp"
   },
   {
     name: "Testimonials section",
-    url: "https://mahidqureshi.me/testimonials",
+    url: "https://mahad410.github.io/testimonials",
     description: "Testimonials section using flexbox",
     img: "/pics/projects/testimonials.webp"
   },
   {
     name: "Stats preview card component",
-    url: "https://mahidqureshi.me/stats",
+    url: "https://mahad410.github.io/stats",
     description: "Modern stats preview card component",
     img: "/pics/projects/stats.webp"
   },
   {
     name: "Quad feature section",
-    url: "https://mahidqureshi.me/quad",
+    url: "https://mahad410.github.io/quad",
     description: "Four card feature section using grid",
     img: "/pics/projects/quad.webp"
   },
   {
     name: "Ping launching soon page",
-    url: "https://mahidqureshi.me/ping",
+    url: "https://mahad410.github.io/ping",
     description: "Ping launching soon page",
     img: "/pics/projects/ping.webp"
   },
   {
     name: "Huddle landing page",
-    url: "https://mahidqureshi.me/huddle",
+    url: "https://mahad410.github.io/huddle",
     description: "Huddle landing page",
     img: "/pics/projects/huddle.webp"
   },
   {
     name: "Discord Clone",
-    url: "https://mahidqureshi.me/discord",
+    url: "https://mahad410.github.io/discord",
     description: "Discord landing page clone with login page",
     img: "/pics/projects/discord.webp"
   },
   {
     name: "Spotify Clone",
-    url: "https://mahidqureshi.me/spotify",
+    url: "https://mahad410.github.io/spotify",
     description: "Spotify landing page clone with login page",
     img: "/pics/projects/spotify.webp"
   },
   {
     name: "Twitter Clone",
-    url: "https://mahidqureshi.me/spotify",
+    url: "https://mahad410.github.io/spotify",
     description: "Twitter home and profile page clone",
     img: "/pics/projects/twitter.webp"
   },
   {
     name: "Batman themed parallax site",
-    url: "https://mahidqureshi.me/batman",
+    url: "https://mahad410.github.io/batman",
     description: "Batman themed site with carousel and parallax effect",
     img: "/pics/projects/batman.webp"
   },
   {
     name: "Halloween themed site",
-    url: "https://mahidqureshi.me/halloweenproject",
+    url: "https://mahad410.github.io/halloweenproject",
     description: "Halloween themed site with animations",
     img: "/pics/projects/halloween.webp"
   },
   {
     name: "Goodwork.co",
-    url: "https://mahidqureshi.me/goodwork.co",
+    url: "https://mahad410.github.io/goodwork.co",
     description: "Goodwork.co landing page with embedded Jotform",
     img: "/pics/projects/goodwork.webp"
   },
   {
     name: "Portfolio site",
-    url: "https://mahidqureshi.me/porti",
+    url: "https://mahad410.github.io/porti",
     description: "Modern portfolio site with animations and stats",
     img: "/pics/projects/portfolio.webp"
   },
   {
     name: "Advice Generator",
-    url: "https://mahidqureshi.me/advice",
+    url: "https://mahad410.github.io/advice",
     description: "Advice Generator with API",
     img: "/pics/projects/advice.webp"
   },
   {
     name: "Modern Notification Component",
-    url: "https://mahidqureshi.me/notification",
+    url: "https://mahad410.github.io/notification",
     description: "Modern news site concept",
     img: "/pics/projects/notification.webp"
   },
   {
     name: "Modern Chart Component",
-    url: "https://mahidqureshi.me/chart",
+    url: "https://mahad410.github.io/chart",
     description: "Modern chart component with Chart.js",
     img: "/pics/projects/chart.webp"
   },
   {
     name: "Price Component",
-    url: "https://mahidqureshi.me/price",
+    url: "https://mahad410.github.io/price",
     description: "Price component using Grid system",
     img: "/pics/projects/price.webp"
   },
   {
     name: "Form Component",
-    url: "https://mahidqureshi.me/form",
+    url: "https://mahad410.github.io/form",
     description: "Form Component with footer",
     img: "/pics/projects/form.webp"
   }
